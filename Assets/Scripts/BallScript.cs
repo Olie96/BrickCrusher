231d54cf00b032e37492765a49e47a4842caf691
Olie96 <breza.oliver@gmail.com>
2017-03-22 16:23:26 +0100

    Score

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody>().AddForce(0,600f,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
---
+++ Assets/Scripts/PaddleScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetAxis("Horizontal") < 0)
+        { 
+            Debug.Log("Left");
+           transform.Translate(-8f*Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            Debug.Log("Right");
+            transform.Translate(8f * Time.deltaTime, 0, 0);
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        foreach (ContactPoint contact in col.contacts)
+        {
+            if (contact.thisCollider == GetComponent<Collider>())
+            {
+               float english = contact.point.x - transform.position.x;
+                contact.otherCollider.GetComponent<Rigidbody>().AddForce(300f*english,0,0);
+            }
+        }
+    }
+}
